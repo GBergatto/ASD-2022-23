7fc2d72b7047f2cdeb61a2681a99eba9cff13c3c
GBergatto <gb.bergatto@gmail.com>
2022-11-25 09:39:07 +0100

    Fixed es3 lab4

--- L04/E03/e03.c
+++ L04/E03/e03.c
@@ -8,60 +8,41 @@ enum pietra { zaffiro,
               topazio,
               smeraldo };
 
-// non ho una lunghezza target!!
-
-// parto da zero a salire
-// finché esiste una collana lunga k, provo k+1
-
-// parto dalla lunghezza massima (somma di tutte le gemme) a scendere
-// contro: devo esplorare spazi molto grandi
-
-// approccio dicotomico: parto da metà, se esiste provo nella metà del
-// sottovettore superiore, altrimenti di quello inferiore
-
-// meno funzioni per non aumentare troppo il tempo di esecuzione
-
-// controlla la validità il prima possibile!! PRUNING
-
 // cerca una collana valida di lunghezza len e restituisce 1 se la trova
-int collana(int pos, int *mark, int *sol, int len, int found) {
+int collana(int pos, int *mark, int *sol, int *best, int len, int found) {
     int check;
-    // terminazione
     if (pos == len) {
-        // for (int i = 0; i < len; i++)
-        //     printf("%d ", sol[i]);
-        // printf("\n");
+        // aggiorno soluzione migliore
+        for (int i = 0; i < pos; i++)
+            best[i] = sol[i];
 
-        return 1;
-        // è sufficiente una soluzione!!
-        // esci da tutte le ricorsioni => flag? if (found == 1) return 1;
-        // vedi teoria
+        return 1;  // flag di successo: basta una soluzione
     }
     // itero sulle 4 pietre
     for (int i = 0; i < 4; i++) {
-        // controllo che la pietra sia ancora disponibile e che possa seguire quella precedente
-        switch (i) {
-            case zaffiro:
-                check = (sol[pos] == zaffiro || sol[pos] == topazio);
-                break;
-            case smeraldo:
-                check = (sol[pos] == smeraldo || sol[pos] == rubino);
-                break;
-            case rubino:
-                check = (sol[pos] == topazio || sol[pos] == zaffiro);
-                break;
-            case topazio:
-                check = (sol[pos] == smeraldo || sol[pos] == rubino);
-                break;
+        if (pos > 0) {  // controllo che la pietra possa seguire la precedente (se non è la prima)
+            switch (i) {
+                case zaffiro:
+                    check = (sol[pos - 1] == zaffiro || sol[pos - 1] == topazio);
+                    break;
+                case smeraldo:
+                    check = (sol[pos - 1] == smeraldo || sol[pos - 1] == rubino);
+                    break;
+                case rubino:
+                    check = (sol[pos - 1] == topazio || sol[pos - 1] == zaffiro);
+                    break;
+                case topazio:
+                    check = (sol[pos - 1] == smeraldo || sol[pos - 1] == rubino);
+                    break;
+            }
         }
-        if (mark[i] > 0 && check) {
+        if (mark[i] > 0 && check) {  // la pietra è presente e valida
             sol[pos] = i;
             mark[i]--;
-            found = collana(pos + 1, mark, sol, len, found);
+            found = collana(pos + 1, mark, sol, best, len, found);
+            mark[i]++;  // restituisco la pietra
             if (found == 1) return 1;
-            mark[i]++;  // restituisco la pietra}
         }
-        // else ?? provo un'altra pietra
     }
     return found;
 }
@@ -69,7 +50,7 @@ int collana(int pos, int *mark, int *sol, int len, int found) {
 int main(int argc, char const *argv[]) {
     int nTest, found, t = 0, maxLen = 0, nPietre = 0;
     int mark[4];  // tiene traccia delle pietre rimaste
-    int *sol;
+    int *sol, *best;
     FILE *f = fopen(FILENAME, "r");
     fscanf(f, "%d", &nTest);
 
@@ -81,26 +62,28 @@ int main(int argc, char const *argv[]) {
             nPietre += mark[i];
         }
         sol = (int *)malloc(nPietre * sizeof(int));
-
-        printf("Test #%d\n", t);
-        printf("zaffiri = %d, rubini = %d, topazi = %d, smeraldi = %d, TOT = %d\n", mark[zaffiro], mark[rubino], mark[topazio], mark[smeraldo], nPietre);
+        best = (int *)malloc(nPietre * sizeof(int));
 
         // approccio dicotomico alla ricerca della lunghezza max
         int a = 0, b = nPietre, k;
         while (a <= b) {
             k = (a + b) / 2;
-            found = collana(0, mark, sol, k, 0);
+            found = collana(0, mark, sol, best, k, 0);
             if (found) {  // aggiorno maxLen e cerco una lunghezza maggiore
                 maxLen = k;
                 a = k + 1;
             } else {  // provo con una lunghezza minore
                 b = k - 1;
             }
-            // printf("Collane di len %d: %d\n", k, found);
         }
-        printf("Lunghezza max: %d\n", maxLen);
 
-        free(sol);  // use realloc instead??
+        printf("#%d Lunghezza max: %d\n", t, maxLen);
+        char intToChar[4] = {'z', 't', 'r', 's'};
+        for (int i = 0; i < maxLen; i++)
+            printf("%c ", intToChar[best[i]]);
+        printf("\n\n");
+
+        free(sol);
     }
     fclose(f);
     return 0;
